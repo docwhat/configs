0f78a79ad2ed8963806cb268674f46a0c1f457aa
root <docwhat@gerf.org>
2019-09-13 18:32:31 -0400

    label writer

---
+++ labels.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eu
+
+declare -ra gerf_add=(
+  letsencrypt.well-known-mount=true
+  swarmpit.swarmpit-data=true
+  swarmpit.swarmpit-logs=true
+)
+
+declare -ra gerf_rm=(
+  portainer.portainer-data=true
+)
+
+### MAIN
+
+for label in "${gerf_rm[@]}"; do
+  docker node update --label-rm "${label}" gerf.org || :
+done
+
+for label in "${gerf_add[@]}"; do
+  docker node update --label-add "${label}" gerf.org
+done
+
+# EOF
